cc0e2b8e939af934804a6a63aa92f7b5c7e5b184
glgnkim <gahngnin.kim@gordon.edu>
2020-06-19 12:16:09 -0400

    Removed unused codes

--- wxPython_test.py
+++ wxPython_test.py
@@ -1,39 +1,10 @@
+
+# Simple picture tool
+# CS Summer Practicum 2020
+# Author: Gahngnin Kim
+
 import os
 import wx
-"""
-# Simplist Hello World app
-class MyFrame(wx.Frame):    
-    def __init__(self):
-        super().__init__(parent=None, title='Hello World')
-        panel = wx.Panel(self)
-
-        self.text_ctrl = wx.TextCtrl(panel, pos=(5, 5))
-        my_btn = wx.Button(panel, label='Press Me', pos=(5, 55))
-
-        self.Show()
-"""
-"""
-# Less simple Hellow World app
-class MyFrame(wx.Frame):    
-    def __init__(self):
-        super().__init__(parent=None, title='Hello World')
-        panel = wx.Panel(self)        
-        my_sizer = wx.BoxSizer(wx.VERTICAL)        
-        self.text_ctrl = wx.TextCtrl(panel)
-        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
-        my_btn = wx.Button(panel, label='Press Me')
-        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
-        panel.SetSizer(my_sizer)        
-        self.Show()
-
-if __name__ == '__main__':
-    app = wx.App()
-    frame = MyFrame()
-    app.MainLoop()
-"""
-
-# Simple text editor
-# Will be implemented into a picture tool
 
 class MainWindow(wx.Frame):
     def __init__(self, parent, title):
