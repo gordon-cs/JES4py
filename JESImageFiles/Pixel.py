5d3aba2be8f113b7d977d599d9ba66fb11f92f4c
Nick-Noormand <nick.noormand@gordon.edu>
2020-06-23 21:56:44 -0400

    Update Pixel.py

--- JESImageFiles/Pixel.py
+++ JESImageFiles/Pixel.py
@@ -1,17 +1,18 @@
 from PIL import Image
 class Pixel:
 
-    def __init__(self, picture, x, y):
-        self.picture = picture
+    def __init__(self, image, x, y):
+        self.image = image
         self.x = x
         self.y = y
+        #self.color = color
 
     #  * Method to return a string with information about this picture.
     #  * @return a string with information about the picture such as fileName,
     #  * height and width.
     #  */
     def __str__(self):
-        output = "Pixel: picture {}, x {} y {} ".format(self.picture.getTitle(), self.picture.height, self.picture.width)
+        output = "Pixel: picture {}, x: {} y: {} color:{}".format(self.image, self.x, self.y, self.image.getpixel((self.x,self.y)))
         return output
 
     def getX(self):
@@ -20,32 +21,35 @@ class Pixel:
     def getY(self):
         return self.y
 
-    def getPicture(self):
-        return self.picture
+    def getImage(self):
+        return self.image
 
     def getRed(self):
-        col = self.getColor()
+        col = self.image.getpixel((self.x,self.y))
         return col[0]
 
     def getGreen(self):
-        col = self.getColor()
+        col = self.image.getpixel((self.x,self.y))
         return col[1]
 
     def getBlue(self):
-        col = self.getColor()
+        col = self.image.getpixel((self.x,self.y))
         return col[2]
 
     def setRed(self, value):
-        self.color[0] = value
+        col = self.image.getpixel((self.x,self.y))
+        self.image.putpixel((self.x, self.y),(value, col[1], col[2]))
 
     def setGreen(self, value):
-        self.color[1] = value
+        col = self.image.getpixel((self.x,self.y))
+        self.image.putpixel((self.x, self.y), (col[0], value, col[2]))
 
     def setBlue(self, value):
-        self.color[2] = value
+        col = self.image.getpixel((self.x,self.y))
+        self.image.putpixel((self.x, self.y), (col[0], col[1], value))
 
     def getColor(self):
-        return self.color
+        return self.image.getpixel((self.x,self.y))
 
     def setColor(self, color):
-        self.color = color
+        self.image.putpixel((self.x, self.y), color)
