9d957bf80f177fb5db81e8b9d0fa4f6ce053eace
Nick-Noormand <nick.noormand@gordon.edu>
2020-07-24 16:09:24 -0400

    version 0.0.3

--- setup.py
+++ setup.py
@@ -5,18 +5,22 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jes4py",
-    version="0.0.1",
+    version="0.0.3",
     author="Jonathan Senning",
     author_email="jonathan.senning@gordon.edu",
     description="A python package implementing part of JES's media functionality",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gordon-cs/JES-emulator",
-    packages=setuptools.find_packages(),
+    packages=['jes4py'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.0.0',
+    python_requires='>=3.0.0',install_requires=[
+    'wave',
+    'wxPython',
+    'simpleaudio'
+    ]
 )
